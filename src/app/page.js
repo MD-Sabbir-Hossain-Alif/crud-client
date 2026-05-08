2d05137c61f90529074dfc89563b129608af0ee8
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2026-05-08 21:58:42 +0600

    add some btn & text

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
+import { Button } from "@heroui/react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<h1 className="text-5xl text-green-600 text-center">
-			Hello World! its Sabbir
-		</h1>
+		<div className="container mx-auto flex flex-col justify-center items-center">
+			<h1 className="text-5xl text-blue-600 text-center font-bold mt-25 mb-5">
+				Hello World! its Sabbir
+			</h1>
+			<Link href='/users'>
+				<Button>
+					See user list
+				</Button>
+			</Link>
+			<p className="text-gray-500 mt-4">Please download <Link href='https://github.com/MD-Sabbir-Hossain-Alif/crud-server' className="text-red-500 hover:underline">crud server</Link> first</p>
+		</div>
 	);
 }
